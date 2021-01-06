974baf9198b4e7ddcf26f055a6aad7b59d092d78
haruka <tomobe.haruka.58m@st.kyoto-u.ac.jp>
2021-01-06 14:55:54 +0900

    updated

--- data/033_okayama/長谷下池/server.f90
+++ data/033_okayama/長谷下池/server.f90
@@ -2,7 +2,7 @@ program main
     use plantFEM
     implicit none
 
-    type(FEMDomain_) :: nagayaike_dam
+    type(FEMDomain_) :: dam
     real(real64) :: x(7),altitude(7),top
     integer(int32) :: i
 
@@ -15,11 +15,11 @@ program main
 
     ! 以下、メッシュ分割プログラム
     ! 要素分割数
-    call nagayaike_dam%create(meshtype="Cube",x_num=30,y_num=30,z_num=12)
+    call dam%create(meshtype="Cube",x_num=30,y_num=30,z_num=12)
     ! 堤体寸法(上下流方向長,堤長, 堤高)
     !15:6.1=x:30.1
-    call nagayaike_dam%resize(x=85.00d0,y=137.0d0,z=3.0d0)
-    call nagayaike_dam%move(x=-5.0d0,z=-2.0d0)
+    call dam%resize(x=85.00d0,y=137.0d0,z=3.0d0)
+    call dam%move(x=-5.0d0,z=-2.0d0)
     ! 上流からx(n) m 進んだ地点の高さがaltitude(n) m
     x(1) = 0.00d0; altitude(1) = 1.00d0;
     x(2) = 7.37d0; altitude(2) = 9.22d0;
@@ -29,10 +29,11 @@ program main
     x(6) =74.01d0; altitude(6) = 8.60d0;
     x(7) =75.73d0; altitude(7) = 1.00d0;
     
-    call nagayaike_dam%edit(x=x, altitude=altitude)
+    call dam%edit(x=x, altitude=altitude)
 
     ! 保存
-    call nagayaike_dam%msh("長谷下池")
+    call dam%msh("長谷下池")
+    call dam%stl("長谷下池")
 
 
 
